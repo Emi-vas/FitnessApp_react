12992a464388baa5ced72cebc15ed2265b2bfdeb
Emilien Vaschalde <emilienvaschalde@yahoo.fr>
2022-08-30 13:21:03 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import Exercise from "./pages/Exercise";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
    <Box width="400px">
-      App
+      <Navbar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<Exercise />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
    </Box>
   );
 };
